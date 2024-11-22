d8d12a9345d4408768e3d2304c3c36f688bd8795
5alomao <salomao.junior2k22@gmail.com>
2024-11-22 15:46:12 -0300

    Conetando a função lambda ao bucket do Amazon S3. Salvando JSON no S3. Gerando UUID da URL encurtada. Adicionando dependências do Amazon S3.

--- src/main/java/com/rocketseat/createUrlShortner/Main.java
+++ src/main/java/com/rocketseat/createUrlShortner/Main.java
@@ -3,14 +3,19 @@ package com.rocketseat.createUrlShortner;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-public class Main implements RequestHandler<Map<String,Object>, Map<String, String>> {
+public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private final S3Client s3Client = S3Client.builder().build();
 
     @Override
     public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
@@ -20,13 +25,28 @@ public class Main implements RequestHandler<Map<String,Object>, Map<String, Stri
         try {
             bodyMap = objectMapper.readValue(body, Map.class);
         } catch (Exception exception) {
-            throw new RuntimeException("Error parsing JSON body: " +exception.getMessage(), exception);
+            throw new RuntimeException("Error parsing JSON body: " + exception.getMessage(), exception);
         }
 
         String originalUrl = bodyMap.get("originalUrl");
         String expirationTime = bodyMap.get("expirationTime");
+        long expirationTimeInSeconds = Long.parseLong(expirationTime);
 
-        String shortUrlCode = UUID.randomUUID().toString().substring(0,8);
+        String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
+
+        UrlData urlData = new UrlData(originalUrl, expirationTimeInSeconds);
+
+        try {
+            String urlDataJson = objectMapper.writeValueAsString(urlData);
+            PutObjectRequest request = PutObjectRequest.builder()
+                    .bucket("url-shortner-api-storage")
+                    .key(shortUrlCode + ".json")
+                    .build();
+
+            s3Client.putObject(request, RequestBody.fromString(urlDataJson));
+        } catch (Exception exception) {
+            throw new RuntimeException("Error saving URL data to S3: " + exception.getMessage(), exception);
+        }
 
         Map<String, String> response = new HashMap<>();
         response.put("code", shortUrlCode);
